391827ddf5cfa41f06dc11c869936fad2c963f9f
FatirKunz <72424634+FatirKunz@users.noreply.github.com>
2024-12-14 11:29:39 +0700

    minor change in multer

--- utils/multer.js
+++ utils/multer.js
@@ -1,11 +1,8 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname.trim().replace(/\s+/g, "")}`);
+    cb(null, file.originalname);
   },
 });
 
