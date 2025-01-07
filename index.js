3f7a718f3b8fcb3174e158a062cc4ca9c8acdc8a
FatirKunz <72424634+FatirKunz@users.noreply.github.com>
2025-01-07 10:28:33 +0700

    .

--- index.js
+++ index.js
@@ -1,53 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const dbConnect = require("./dbConnect");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const dbConnect = require('./dbConnect')
 
-const productRoute = require("./routes/productRoute");
-const userRoute = require("./routes/userRoute");
-const commentRoute = require("./routes/commentRoute");
-const orderRoute = require("./routes/orderRoute");
-const reservationRoute = require("./routes/reservationRoute");
-const tableRoute = require("./routes/tableRoute");
-const commonRoute = require("./routes/commonRoute");
+const productRoute = require('./routes/productRoute')
+const userRoute = require('./routes/userRoute')
+const commentRoute = require('./routes/commentRoute')
+const orderRoute = require('./routes/orderRoute')
+const reservationRoute = require('./routes/reservationRoute')
+const tableRoute = require('./routes/tableRoute')
+const commonRoute = require('./routes/commonRoute')
 
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Origin",
-      "Accept",
-    ], 
-  })
-);
-
-
-dbConnect();
-
-app.use("/api", productRoute);
-app.use("/api", userRoute);
+const app = express()
+const port = process.env.PORT || 8080
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: true}))
+app.use('/api', productRoute)
+app.use('/api', userRoute)
 app.use("/api", commentRoute);
-app.use("/api", orderRoute);
-app.use("/api", tableRoute);
-app.use("/api", reservationRoute);
-app.use("/api", commonRoute);
+app.use("/api", orderRoute)
+app.use("/api", tableRoute)
+app.use("/api", reservationRoute)
+app.use("/api", commonRoute)
+dbConnect()
 
-app.get("/", (req, res) => {
-  res.send("Cafe API");
-});
+app.get('/', (req, res) => {
+    res.send("Cafe Api")
+})
 
 app.listen(port, () => {
-  console.log(`Listening at ${port}`);
-});
+    console.log(`Listening at ${port}`)
+})
